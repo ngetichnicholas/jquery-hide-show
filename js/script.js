6adbf6cafff294a099f54d9ff98b4c41bacd2b26
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-04-20 11:32:21 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@ $(document).ready(function() {
   $(".define").hide();    
   $(".language").click(function(){
     $(this).siblings().show();
+    $(this).hide();
+
   });
   $(".define").click(function(){
     $(this).hide();
+    $(this).siblings().show();
   });
 });
  
